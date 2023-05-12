e9f3569544a4e6b6a83ad6507fb88763c33c2507
pcbennett108 <pcbennett42@gmail.com>
2023-05-11 19:14:04 -0600

    Create first 3 tests for Node class

---
+++ spec/node_spec.rb
@@ -0,0 +1,21 @@
+require './lib/node'
+
+RSpec.describe Node do
+  it 'exists' do
+    node = Node.new("plop")
+
+    expect(node).to be_instance_of(Node)
+  end
+
+  it 'has data' do
+    node = Node.new("plop")
+
+    expect(node.data).to eq("plop")
+  end
+
+  it 'has a nil next node' do
+    node = Node.new("plop")
+    expect(node.next_node).to eq(nil)
+  end
+
+end
